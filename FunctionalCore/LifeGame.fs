b4973134b12d6604639310d55f8e429dfe1b0691
Dallan Simper <dallan.simper@gmail.com>
2013-03-15 20:46:30 -0600

    Add Game of Life

---
+++ FunctionalCore/LifeGame.fs
@@ -0,0 +1,9 @@
+﻿module LifeGame
+
+
+
+
+let Advance A =
+    Array2D.init 
+        (Array2D.length1 A) (Array2D.length2 A)
+        (fun x y -> A.[x,y])
---
+++ ObjectiveShell.UnitTests/LifeGameTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ObjectiveShell.UnitTests
+{
+    [TestFixture]
+    public class LifeGameTests
+    {
+        [Test]
+        public void DeadBoardStillDead()
+        {
+            var board = new[,]
+                            {
+                                {0, 0, 0},
+                                {0, 0, 0},
+                                {0, 0, 0},
+                            };
+            Assert.That(LifeGame.Advance(board), Is.EqualTo(board));
+        }
+    }
+}
