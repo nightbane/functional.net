02bd0a4baa1503a169f9a689da92370cae24da3c
Dallan Simper <dallan.simper@gmail.com>
2013-03-15 23:21:47 -0600

    refactor LifeGame

--- FunctionalCore/LifeGame.fs
+++ FunctionalCore/LifeGame.fs
@@ -1,23 +1,31 @@
 ﻿module LifeGame
 
 let IsTopRow A x = x = 0
-let IsBottomRow (A: int[,]) x = (Array2D.length1 A - 1) = x
-let IsMiddleColumn (A: int[,]) y = y > 0 && (Array2D.length2 A) - 1 > y
+let IsBottomRow A x = (Array2D.length1 A) - 1 = x
+let IsMiddleColumn A y = y > 0 && (Array2D.length2 A) - 1 > y
 let IsFirstColumn A y = y = 0
 
 let GetRowCount (A: int[,]) x y = 
+    A.[x,y] + 
     if IsMiddleColumn A y 
-        then A.[x,y-1] + A.[x,y] + A.[x,y+1]
+        then A.[x,y-1] + A.[x,y+1]
     else if IsFirstColumn A y
-        then A.[x,y] + A.[x,y+1]
-    else A.[x,y-1] + A.[x,y]
+        then A.[x,y+1]
+    else A.[x,y-1]
 
-let CountNeighbors (A: int[,]) x y =
-    (if IsTopRow A x then 0 else GetRowCount A (x-1) y) +
-    ((GetRowCount A x y) - A.[x,y]) +
-    (if IsBottomRow A x then 0 else GetRowCount A (x+1) y)
+let CountFirstRow A x y =
+    if IsTopRow A x then 0 else GetRowCount A (x-1) y
 
+let CountMiddleRow A x y =
+    GetRowCount A x y - A.[x,y]
 
+let CountBottomRow A x y =
+    if IsBottomRow A x then 0 else GetRowCount A (x+1) y
+
+let CountNeighbors A x y =
+    CountFirstRow A x y +
+    CountMiddleRow A x y +
+    CountBottomRow A x y
 
 let Advance A =
     Array2D.init 
