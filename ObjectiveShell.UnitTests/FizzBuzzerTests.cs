a8b7169f91008b051468f99ce42bd9929518bf9a
Dallan Simper <dallan.simper@gmail.com>
2013-03-15 20:29:05 -0600

    name change

--- ObjectiveShell.UnitTests/FizzBuzzerTests.cs
+++ ObjectiveShell.UnitTests/FizzBuzzerTests.cs
@@ -7,7 +7,7 @@ using NUnit.Framework;
 namespace ObjectiveShell.UnitTests
 {
     [TestFixture]
-    public class FizzBuzzTests
+    public class FizzBuzzerTests
     {
         [Test]
         public void TestOneisOne()
