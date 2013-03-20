9b01381eafa8b33676f08eee8890db7e4093be73
Dallan Simper <dallan.simper@gmail.com>
2013-03-20 11:35:35 -0600

    remove list access

--- ObjectiveShell/Default.aspx.cs
+++ ObjectiveShell/Default.aspx.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using System.Linq;
 using System.Web.UI;
-using ObjectiveShell.UnitTests;
 
 namespace ObjectiveShell
 {
     public partial class Default : Page
     {
-        private readonly FSharpListHelper listhelper = new FSharpListHelper();
-
         protected void Page_Load(object sender, EventArgs e)
         {
-            var sharplist = FizzBuzzer.TranslateList(FizzBuzzer.HundredList);
-            var list = listhelper.ConvertList(sharplist);
-            litOutput.Text = list.Aggregate(string.Empty, (current, item) => current + (item + "<br />\n"));
+            var fsharplist = FizzBuzzer.TranslateList(FizzBuzzer.HundredList);
+            litOutput.Text = fsharplist.Aggregate(string.Empty, (current, item) => current + (item + "<br />\n"));
         }
     }
 }
